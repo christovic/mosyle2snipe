9dd2deb77de0f5f2038137ecec817d795f1f4ceb
christovic <danielchristovic@gmail.com>
2023-05-04 10:36:07 +0100

    small changes & fixes

--- mosyle2snipe.py
+++ mosyle2snipe.py
@@ -11,16 +11,16 @@ import mosyle_api
 
 # Set us up for using runtime arguments by defining them.
 runtimeargs = argparse.ArgumentParser()
-runtimeargs.add_argument("--mdm", help="Sets the MDM system you'll be using, either mosyle or mosyle")
+runtimeargs.add_argument("--mdm", help="Sets the MDM system you'll be using, either Mosyle or Mosyle")
 runtimeargs.add_argument("-v", "--verbose", help="Sets the logging level to INFO and gives you a better idea of what the script is doing.", action="store_true")
-runtimeargs.add_argument("--dryrun", help="This checks your config and tries to contact both the mosylePro and Snipe-it instances, but exits before updating or syncing any assets.", action="store_true")
+runtimeargs.add_argument("--dryrun", help="This checks your config and tries to contact both the MosylePro and Snipe-it instances, but exits before updating or syncing any assets.", action="store_true")
 runtimeargs.add_argument("-d", "--debug", help="Sets logging to include additional DEBUG messages.", action="store_true")
 runtimeargs.add_argument('--do_not_verify_ssl', help="Skips SSL verification for all requests. Helpful when you use self-signed certificate.", action="store_false")
 runtimeargs.add_argument("-r", "--ratelimited", help="Puts a half second delay between Snipe IT API calls to adhere to the standard 120/minute rate limit", action="store_true")
 user_opts = runtimeargs.add_mutually_exclusive_group()
-user_opts.add_argument("-u", "--users", help="Checks out the item to the current user in mosyle if it's not already deployed", action="store_true")
-user_opts.add_argument("-ui", "--users_inverse", help="Checks out the item to the current user in mosyle if it's already deployed", action="store_true")
-user_opts.add_argument("-uf", "--users_force", help="Checks out the item to the user specified in mosyle no matter what", action="store_true")
+user_opts.add_argument("-u", "--users", help="Checks out the item to the current user in Mosyle if it's not already deployed", action="store_true")
+user_opts.add_argument("-ui", "--users_inverse", help="Checks out the item to the current user in Mosyle if it's already deployed", action="store_true")
+user_opts.add_argument("-uf", "--users_force", help="Checks out the item to the user specified in Mosyle no matter what", action="store_true")
 type_opts = runtimeargs.add_mutually_exclusive_group()
 type_opts.add_argument("-m", "--mobiles", help="Runs mobiles only", action="store_true")
 type_opts.add_argument("-c", "--computers", help="Runs computers only", action="store_true")
@@ -70,7 +70,7 @@ logging.debug("The API key you provided for Snipe is: {}".format(apiKey))
 defaultStatus = config['snipe-it']['defaultStatus']
 logging.info("The default status we'll be setting updated computer to is: {} (I sure hope this is a number or something is probably wrong)".format(defaultStatus))
 apple_manufacturer_id = config['snipe-it']['manufacturer_id']
-logging.info("The configured mosylePro base url is: {} (Pretty sure this needs to be a number too)".format(apple_manufacturer_id))
+logging.info("The configured Mosyle base url is: {}".format(apple_manufacturer_id))
 
 snipeheaders = {'Authorization': 'Bearer {}'.format(apiKey),'Accept': 'application/json','Content-Type':'application/json'}
 
@@ -126,7 +126,7 @@ def search_snipe_asset(serial):
         # Check to make sure there's actually a result
         if 'rows' in jsonresponse:
             if jsonresponse['total'] == 1:
-                return jsonresponse
+                return jsonresponse['rows'][0]
             else:
                 logging.warning('FOUND {} matching assets while searching for: {}'.format(jsonresponse['total'], serial))
                 return "MultiMatch"
@@ -170,48 +170,28 @@ def get_snipe_models():
 def get_snipe_assets(category):
     api_url = '{}/api/v1/hardware'.format(snipe_base)
     count = 0
+    all_models = {}
     while True:
         response = requests.get(api_url, headers=snipeheaders,
             verify=user_args.do_not_verify_ssl,
             hooks={'response': request_handler},
-            params={'category_id': category, 'limit': 50})
+            params={'category_id': category, 'limit': 100, 'offset': count})
         if response.status_code == 200:
+            logging.debug("Got good page of Snipe assets")
             jsonresponse = response.json()
+            for device in jsonresponse['rows']:
+                if device['serial'] in all_models:
+                    logging.warn(f"{device['serial']} is a duplicate serial number for asset id {device['id']}")
+                all_models[device['serial']] = device
             if len(jsonresponse['rows']) + count == jsonresponse['total']:
                 break
+            count += len(jsonresponse['rows'])
         else:
+            logging.error(f"Could not get a list of assets from Snipe: {response.status_code}: {response.text}")
+            raise SystemExit()
             break
 
-
-
-
-
-
-    api_url = '{}/api/v1/hardware'.format(snipe_base)
-    logging.debug('Calling against: {}'.format(api_url))
-    response = requests.get(api_url, headers=snipeheaders, verify=user_args.do_not_verify_ssl, hooks={'response': request_handler})
-    if response.status_code == 200:
-        jsonresponse = response.json()
-        logging.debug("Got a valid response that should have {} models.".format(jsonresponse['total']))
-        if jsonresponse['total'] <= len(jsonresponse['rows']) :
-            return jsonresponse
-        else:
-            logging.info("We didn't get enough results so we need to get them again.")
-            api_url = '{}/api/v1/models?limit={}'.format(snipe_base, jsonresponse['total'])
-            newresponse = requests.get(api_url, headers=snipeheaders, verify=user_args.do_not_verify_ssl, hooks={'response': request_handler})
-            if response.status_code == 200:
-                newjsonresponse = newresponse.json()
-                if newjsonresponse['total'] == len(newjsonresponse['rows']) :
-                    return newjsonresponse
-                else:
-                    logging.error("We couldn't seem to get all of the model numbers")
-                    raise SystemExit("Unable to get all model objects from Snipe-IT instanace")
-            else:
-                logging.error('When we tried to retreive a list of models, Snipe-IT responded with error status code:{} - {}'.format(response.status_code, response.content))
-                raise SystemExit("Snipe models API endpoint failed.")
-    else:
-        logging.error('When we tried to retreive a list of models, Snipe-IT responded with error status code:{} - {}'.format(response.status_code, response.content))
-        raise SystemExit("Snipe models API endpoint failed.")
+    return all_models
 
 # Function to search snipe for a user 
 def get_snipe_user_id(username):
@@ -284,8 +264,9 @@ def update_snipe_asset(snipe_id, payload):
         logging.debug("Got back status code: 200 - Checking the payload updated properly: If you error here it's because you configure the API mapping right.")
         jsonresponse = response.json()
         for key in payload:
-            if jsonresponse['payload'][key] != payload[key]:
-                logging.warning('Unable to update ID: {}. We failed to update the {} field with "{}"'.format(snipe_id, key, payload[key]))
+            payload_key = None if payload[key] == "" else payload[key]
+            if jsonresponse['payload'][key] != payload_key:
+                logging.warning('Unable to update ID: {}. We failed to update the {} field with "{}".'.format(snipe_id, key, payload[key]))
                 goodupdate = False
             else:
                 logging.info("Sucessfully updated {} with: {}".format(key, payload[key]))
@@ -355,7 +336,7 @@ logging.info("SSL Verification is set to: {}".format(user_args.do_not_verify_ssl
 # Do some tests to see if the hosts are up.
 logging.info("Running tests to see if hosts are up.")
 try:
-    SNIPE_UP = True if requests.get(snipe_base, verify=user_args.do_not_verify_ssl, hooks={'response': request_handler}).status_code is 200 else False
+    SNIPE_UP = True if requests.get(snipe_base, verify=user_args.do_not_verify_ssl, hooks={'response': request_handler}).status_code == 200 else False
 except:
     SNIPE_UP = False
 if SNIPE_UP is False:
@@ -372,13 +353,16 @@ logging.info("Finished running our tests.")
 # Get a list of known models from Snipe
 logging.info("Getting a list of computer models that snipe knows about.")
 snipemodels = get_snipe_models()
+logging.info("Getting a list of Snipe assets to reference from.")
 snipe_computer_assets = get_snipe_assets(config['snipe-it']['computer_model_category_id'])
 snipe_mobile_assets = get_snipe_assets(config['snipe-it']['mobile_model_category_id'])
+all_snipe_assets = snipe_computer_assets | snipe_mobile_assets
+logging.info(f"Got a list of {len(snipe_computer_assets)} computers, {len(snipe_mobile_assets)} mobile devices, for a total of {len(all_snipe_assets)} assets from Snipe IT to reference from")
 logging.debug("Parsing the {} model results for models with model numbers.".format(len(snipemodels['rows'])))
 modelnumbers = {}
 modelnames = []
 for model in snipemodels['rows']:
-    if model['model_number'] is "":
+    if model['model_number'] == "":
         logging.debug("The model, {}, did not have a model number. Skipping.".format(model['name']))
         continue
     modelnumbers[model['model_number']] =  model['id']
@@ -403,11 +387,11 @@ else:
         TotalNumber += len(mosyle_types[mosyle_type]) 
 
 # Make sure we have a good list.
-if mosyle_computer_list is not None:
+if mosyle_computer_list is not None and mosyle_mobile_list is not None:
     logging.info('Received a list of Mosyle assets that had {} entries.'.format(TotalNumber))
 else:
-    logging.error("We were not able to retreive a list of assets from your mosyle instance. It's likely that your settings, or credentials are incorrect. Check your settings.conf and verify you can make API calls outside of this system with the credentials found in your settings.conf")
-    raise SystemExit("Unable to get mosyle Computers.")
+    logging.error("We were not able to retreive a list of assets from your Mosyle instance. It's likely that your settings, or credentials are incorrect. Check your settings.conf and verify you can make API calls outside of this system with the credentials found in your settings.conf")
+    raise SystemExit("Unable to get Mosyle Computers.")
 
 # After this point we start editing data, so quit if this is a dryrun
 if user_args.dryrun:
@@ -443,14 +427,14 @@ for mosyle_type in mosyle_types:
             logging.info(f"Could not match the model name from Mosyle {md['device_model_name']} in Snipe, it must have changed.")
             update_snipe_model(model_id, update_model)
 
-        # Pass the SN from mosyle to search for a match in Snipe
-        snipe = search_snipe_asset(md['serial_number'])
+        # Pass the SN from Mosyle to search for a match in Snipe
+        snipe = all_snipe_assets.get(md['serial_number'])
 
         # Create a new asset if there's no match:
-        if snipe is 'NoMatch':
-            logging.info("Creating a new asset in snipe for mosyle ID {} - {}".format(md['serial_number'], md['device_name']))
+        if snipe is None:
+            logging.info("Creating a new asset in snipe for Mosyle ID {} - {}".format(md['serial_number'], md['device_name']))
             # This section checks to see if the asset tag was already put into mosyle, if not it creates one with with mosyle's ID.
-            if md['asset_tag'] is '':
+            if md['asset_tag'] == '':
                 mosyle_asset_tag = md['serial_number']
             else:
                 mosyle_asset_tag = md['asset_tag']
@@ -478,20 +462,12 @@ for mosyle_type in mosyle_types:
                     checkout_snipe_asset(md[config['user-mapping']['mosyle_api_field']],new_snipe_asset[1].json()['payload']['id'], "NewAsset")
             snipe = search_snipe_asset(md['serial_number'])
 
-        # Log an error if there's an issue, or more than once match.
-        elif snipe is 'MultiMatch':
-            logging.warning("WARN: You need to resolve multiple assets with the same serial number in your inventory. If you can't find them in your inventory, you might need to purge your deleted records. You can find that in the Snipe Admin settings. Skipping serial number {} for now.".format(md['serial_number']))
-            continue
-        elif snipe is 'ERROR':
-            logging.error("We got an error when looking up serial number {} in snipe, which shouldn't happen at this point. Check your snipe instance and setup. Skipping for now.".format(md['serial_number']))
-            continue
-
-        # Only update if mosyle has more recent info.
-        logging.info(snipe)
-        snipe_id = snipe['rows'][0]['id']
-        snipe_time = snipe['rows'][0]['updated_at']['datetime']
+        # Only update if Mosyle has more recent info.
+        logging.debug("Snipe asset retreived: " + json.dumps(snipe, indent=4))
+        snipe_id = snipe['id']
+        snipe_time = snipe['updated_at']['datetime']
         mosyle_time = md['date_last_beat']
-        # Check to see that the mosyle record is newer than the previous Snipe update.
+        # Check to see that the Mosyle record is newer than the previous Snipe update.
         #if mosyle_time > snipe_time:
         #if True: # uncomment for testing
         payload = {}
@@ -511,17 +487,17 @@ for mosyle_type in mosyle_types:
             # Need to check that we're not needlessly updating the asset.
             # If it's a custom value it'll fail the first section and send it to except section that will parse custom sections.
             try:
-                if snipe['rows'][0][snipekey] != latestvalue:
+                if snipe[snipekey] != latestvalue:
                     payload[snipekey] = mosyle_value
                 else:
                     logging.debug("Skipping the payload, because it already exits.")
             except:
                 logging.debug("The snipekey lookup failed, which means it's a custom field. Parsing those to see if it needs to be updated or not.")
                 needsupdate = False
-                for CustomField in snipe['rows'][0]['custom_fields']:
-                    if snipe['rows'][0]['custom_fields'][CustomField]['field'] == snipekey :
-                        if snipe['rows'][0]['custom_fields'][CustomField]['value'] != latestvalue:
-                            logging.debug("Found the field, and the value needs to be updated from {} to {}".format(snipe['rows'][0]['custom_fields'][CustomField]['value'], latestvalue))
+                for CustomField in snipe['custom_fields']:
+                    if snipe['custom_fields'][CustomField]['field'] == snipekey :
+                        if snipe['custom_fields'][CustomField]['value'] != latestvalue:
+                            logging.debug("Found the field, and the value needs to be updated from {} to {}".format(snipe['custom_fields'][CustomField]['value'], latestvalue))
                             needsupdate = True
                 if needsupdate is True:
                     payload[snipekey] = mosyle_value
@@ -530,12 +506,12 @@ for mosyle_type in mosyle_types:
         if len(payload) > 0:
             update_snipe_asset(snipe_id, payload)
 
-        if ((user_args.users or user_args.users_inverse) and (snipe['rows'][0]['assigned_to'] == None) == user_args.users) or user_args.users_force:
-            if snipe['rows'][0]['status_label']['status_meta'] in ('deployable', 'deployed'):
-                checkout_snipe_asset(md.get(config['user-mapping']['mosyle_api_field']), snipe_id, snipe['rows'][0]['assigned_to'])
+        if ((user_args.users or user_args.users_inverse) and (snipe['assigned_to'] == None) == user_args.users) or user_args.users_force:
+            if snipe['status_label']['status_meta'] in ('deployable', 'deployed'):
+                checkout_snipe_asset(md.get(config['user-mapping']['mosyle_api_field']), snipe_id, snipe['assigned_to'])
             else:
                 logging.info("Can't checkout {} since the status isn't set to deployable".format(md['device_name']))
         # Update/Sync the Snipe Asset Tag Number back to mosyle
-        if md['asset_tag'] != snipe['rows'][0]['asset_tag']:
+        if md['asset_tag'] != snipe['asset_tag']:
             logging.info("mosyle doesn't have the same asset tag as SNIPE so we'll update it because it should be authoritative.")
-            mosyle.update_devices(md['serial_number'], {"asset_tag": snipe['rows'][0]['asset_tag']})
+            mosyle.update_devices(md['serial_number'], {"asset_tag": snipe['asset_tag']})
